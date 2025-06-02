1bb56b8b90b1d3ccc50a839de52abbceed82d7bb
LeslieLlama <leslieundinecrooks@gmail.com>
2025-06-02 22:21:07 +1200

    Map system
    Added a simple map system to track the player as they progress through rooms. Has the limitation of only showing a rooms co-ordinate, and not it's size, and oddly sized rooms will interfere with the map. Design consideration to look into later.

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -2,9 +2,17 @@ extends CanvasLayer
 
 var pause_menu_on : bool = false
 var dialouge_panel_on : bool = false
+
+var rooms = []
 func _ready() -> void:
 	Signals.collectible_obtained.connect(collectible_aquired)
 	Signals.new_dialouge.connect(_new_dialouge)
+	Signals.new_room_entered.connect(_map_update)
+	for i in 16:
+		var child_node = ColorRect.new()
+		child_node.custom_minimum_size = Vector2(10,10)
+		rooms.append(child_node)
+		$PausePanel/MapPanel/GridContainer.add_child(child_node)
 
 func collectible_aquired(powerupName):
 	#$MoonCounter.text = str("Moons : ",SaveSystem.moons_collected,"/20")
@@ -12,14 +20,15 @@ func collectible_aquired(powerupName):
 		
 func _process(delta: float) -> void:
 	$GlobalTimerMeter.value = 100-($GlobalTimer.time_left * 100)
-	
 	if Input.is_action_just_pressed("start"):
 		if pause_menu_on == false:
 			$PausePanel.visible = true
 			pause_menu_on = true
+			_pause_game(true)
 		else: 
 			$PausePanel.visible = false
 			pause_menu_on = false
+			_pause_game(false)
 			
 	if dialouge_panel_on == true:
 		if Input.is_action_just_pressed("up"):
@@ -34,5 +43,22 @@ func _close_dialouge():
 	$DialougePanel.visible = false
 	dialouge_panel_on = false
 
+func _map_update(_area: Area2D):
+	for i in rooms.size():
+		$PausePanel/MapPanel/GridContainer.get_child(i).color = Color("White")
+	
+	#480,300
+	var pos : Vector2
+	pos.x = _area.position.x/480
+	pos.y = _area.position.y/300
+	
+	var cell = pos.y * $PausePanel/MapPanel/GridContainer.columns
+	cell += pos.x
+	$PausePanel/MapPanel/GridContainer.get_child(cell).color = Color("Red")
+		
+
 func _on_global_timer_timeout() -> void:
 	Signals.emit_signal("TurnEnd")
+	
+func _pause_game(is_pause : bool):
+	get_tree().paused = is_pause
